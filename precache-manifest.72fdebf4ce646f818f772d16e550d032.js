8933894b3018bce1120acd152b445b8c96fd6a70
amenchrist <amenphronesis@gmail.com>
2019-07-28 22:21:37 +0100

    Updates

---
+++ precache-manifest.72fdebf4ce646f818f772d16e550d032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb592ef7bb0f89621884cc4c1f95d934",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "016a47df497c03cfee4d",
+    "url": "/smart-brain/static/css/2.2ec98f22.chunk.css"
+  },
+  {
+    "revision": "f344e15680d3a10afa2c",
+    "url": "/smart-brain/static/css/main.073eeaf2.chunk.css"
+  },
+  {
+    "revision": "016a47df497c03cfee4d",
+    "url": "/smart-brain/static/js/2.de0b2f9f.chunk.js"
+  },
+  {
+    "revision": "f344e15680d3a10afa2c",
+    "url": "/smart-brain/static/js/main.879d4e39.chunk.js"
+  },
+  {
+    "revision": "b9b46f64ddcbca33fcc3",
+    "url": "/smart-brain/static/js/runtime~main.e63bf16e.js"
+  }
+]);
